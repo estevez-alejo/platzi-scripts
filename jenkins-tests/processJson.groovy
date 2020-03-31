6171452e6a7344f111ce85b4ee69cc1a910d098d
alejo <estevez.alejo@gmail.com>
2020-03-31 14:20:51 +0200

    input from alejo sin https

--- jenkins-tests/processJson.groovy
+++ jenkins-tests/processJson.groovy
@@ -13,7 +13,7 @@ private static Map getInputMap() {
     //def inputFile = new File("/var/lib/jenkins/workspace/test-pipeline-sum-node/jenkins-tests/input.json")
    // def inputFile = new File("https://api.myjson.com/bins/10mzec")
     //def inputJSON = new JsonSlurper().parseText(inputFile.text)
-    def inputJSON = new JsonSlurper().parse("https://www.alejoestevez.com/input.json".toURL())
+    def inputJSON = new JsonSlurper().parse("http://www.alejoestevez.com/input.json".toURL())
 
     inputJSON
 }
