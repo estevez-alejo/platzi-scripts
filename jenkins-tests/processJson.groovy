5c95530dd4822977bff68f80e5f5a56406a63950
alejo <estevez.alejo@gmail.com>
2020-03-31 14:33:07 +0200

    refactor for reading json via url

--- jenkins-tests/processJson.groovy
+++ jenkins-tests/processJson.groovy
@@ -11,9 +11,9 @@ import java.text.SimpleDateFormat
 
 private static Map getInputMap() {
     //def inputFile = new File("/var/lib/jenkins/workspace/test-pipeline-sum-node/jenkins-tests/input.json")
-    def inputFile = new File("www.alejoestevez/input.json")
+    //def inputFile = new File("www.alejoestevez/input.json")
     //def inputJSON = new JsonSlurper().parseText(inputFile.text)
-    def inputJSON = new JsonSlurper().parse("www.alejoestevez/input.json".toURL())
+    def inputJSON = new JsonSlurper().parse("http://www.alejoestevez/input.json".toURL())
 
     inputJSON
 }
